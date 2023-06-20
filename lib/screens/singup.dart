b03db9f1757f7b8a77af0e04190149dd1fa03e06
uwu <uwu@upc.edu.pe>
2023-06-20 15:25:50 -0500

    Eddited singup

--- lib/screens/singup.dart
+++ lib/screens/singup.dart
@@ -1,48 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:superhero_flutter/data/remote/http_helper.dart';
+import '../data/remote/user_service.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
+
   @override
   State<SignUp> createState() => _SignUpState();
 }
 
 class _SignUpState extends State<SignUp> {
-  final usernameController = TextEditingController();
-  final passwordController = TextEditingController();
-  final httpHelper = HttpHelper();
+    final _usernameController = TextEditingController();
+    final _passwordController = TextEditingController();
+    final _userService = UserService();
 
   createUser() async {
-    final result = await httpHelper.createUser(
-        usernameController.text, passwordController.text);
+    final result = await _userService.createUser(_usernameController.text, _passwordController.text);
 
-    if (result) {}
+    if (result){
+      print(result);
+    }
   }
-
   @override
   void dispose() {
-    usernameController.dispose();
-    passwordController.dispose();
+    _usernameController.dispose();
+    _passwordController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+
+
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('SuperHero'),
+      ),
         body: Column(
       children: [
         TextField(
-          controller: usernameController,
+          controller: _usernameController,
           onChanged: (value) {},
         ),
         TextField(
-          controller: passwordController,
+          controller: _passwordController,
           onChanged: (value) {},
         ),
         OutlinedButton(
-          onPressed: () {},
-          child: const Text("Sign Up"),
-        ),
+          onPressed: () {
+            createUser();
+          },
+          child: const Text('Sign up'),
+        )
       ],
     ));
   }
